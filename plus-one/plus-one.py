5592a2c2f94877c7994c7533b48622d4cf9e0b03
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-05-16 13:31:58 -0700

    Time: 39 ms, Memory: 13.8 MB - LeetHub

--- plus-one/plus-one.py
+++ plus-one/plus-one.py
@@ -1,11 +1,15 @@
 class Solution:
-    def plusOne(self, numList: List[int]) -> List[int]:
-        print(type(numList))
-        numList = map(str, numList)
-        numList = ''.join(numList)
-        numList = int(numList)
-        numList += 1
-        numList = str(numList)
-        numList = map(int, numList)
-        numList = list(numList)
-        return numList
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits)):
+            digits[i] = str(digits[i])
+
+        digits = ''.join(digits)
+        digits = int(digits)
+        digits += 1
+
+        digits = str(digits)
+        digits = list(digits)
+
+        for i in range(len(digits)):
+            digits[i] = int(digits[i])
+        return digits
