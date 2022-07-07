2fb1be6f1b67fde392d212ca6761a648d1b46a8b
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-07-07 07:37:06 -0700

    Time: 3123 ms, Memory: 25.4 MB - LeetHub

--- rotate-array/rotate-array.py
+++ rotate-array/rotate-array.py
@@ -1,9 +1,11 @@
 class Solution:
-    def rotate(self, numList: List[int], times: int) -> None:
+    def rotate(self, num_list: List[int], times: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
+        given an array, rotate the array to the right by k steps, 
+    where k is non-negative
         """
         for i in range(times):
-            numList.insert(0, numList[-1])
-            numList.pop()
-  
+            to_move = num_list.pop()
+            num_list.insert(0, to_move)
+        
