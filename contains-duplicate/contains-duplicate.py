493bf2735ef9a87f0f437bbf80a93ba33f7a3d8b
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-07-15 09:18:36 -0700

    Update contains-duplicate.py

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -2,11 +2,11 @@ from collections import Counter
 
 class Solution:
     def containsDuplicate(self, num_list: List[int]) -> bool:
-        c = Counter(num_list).values()
+        c = Counter(num_list).values() # creates a list of dictionary values (how many of each number is in the list)
         duplicate = False
-        for num in c:
-            if num > 1:
-                duplicate = True
-                break
+        for num in c: # iterate through the values
+            if num > 1: # if one of the values is greater than 1 (if there are duplicates)
+                duplicate = True 
+                break  # exit loop
         return duplicate
         
