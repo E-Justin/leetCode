4a4c2a6bf6acc7063bd8e68758d7df3b117f07d6
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-08-05 12:26:54 -0700

    Update delete-node-in-a-linked-list.py

--- delete-node-in-a-linked-list/delete-node-in-a-linked-list.py
+++ delete-node-in-a-linked-list/delete-node-in-a-linked-list.py
@@ -9,11 +9,11 @@ class Solution:
         """
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
-        """
-        node.val = node.next.val
-        if node.next.next is not None:
-            node.next = node.next.next
-        else:
-            node.next = None
+        """                                 #        start list                             1 -> (2) -> 3 -> 4         1 -> 2 -> (3) -> 4
+        node.val = node.next.val            # current node == current.next                  1 -> (3) -> 3 -> 4         1 -> 2 -> (4) -> 4
+        if node.next.next is not None:      # if current.next.next is there                 there is data there        no data there
+            node.next = node.next.next      # current.next points to the next, next one     1 -> (3) -> 4              skip
+        else:                               # if the next node is the tail                                             
+            node.next = None                # just remove it                                                           1 -> 2 -> 4 -> None
         
         
