6ac8469eec6afa7043e7f89399e51a1bde1289f5
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-08-18 07:33:35 -0700

    Time: 50 ms, Memory: 13.7 MB - LeetHub

--- plus-one/plus-one.py
+++ plus-one/plus-one.py
@@ -1,18 +1,13 @@
 class Solution:
     def plusOne(self, num_list: List[int]) -> List[int]:
-        new = [str(i) for i in num_list]  # convert list of ints into list of strings
+        
+        for i in range(len(num_list)): num_list[i] = str(num_list[i])
+        num_list = ''.join(num_list)
+        num_list = int(num_list)
+        num_list += 1
 
-        new = ''.join(new)  # convert list of strings to single string
-
-        new = int(new)  # convert string to int
-
-        new += 1  # increment by 1
-
-        new = str(new)  # convert int back to string
-
-        new = list(new)  # convert string into list
-
-        for i in range(len(new)):  # convert each individual item to an int
-            new[i] = int(new[i])
-
-        return new
+        num_list = str(num_list)
+        num_list = list(num_list)
+        for i in range(len(num_list)): num_list[i] = int(num_list[i])
+        
+        return num_list
