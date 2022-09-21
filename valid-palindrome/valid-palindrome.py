ad94b0f5365d0b1c5c1965d37457989d7b14ff40
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-09-21 07:01:49 -0500

    Time: 922 ms, Memory: 14.3 MB - LeetHub

--- valid-palindrome/valid-palindrome.py
+++ valid-palindrome/valid-palindrome.py
@@ -1,15 +1,17 @@
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-            # convert to lowercase
-        s = s.lower()
-        # remove all spaces
-        s = s.replace(' ', '')
-        # remove all non alpha characters
-        for char in s:
-            if not char.isalnum():
-                s = s.replace(char, '')
+    def isPalindrome(self, words: str) -> bool:
 
-        if s == s[::-1]:
-            return True
-        else:
-            return False
+        words = words.lower()
+        palindrome = False
+        backwards = ''
+
+        for char in words:
+            if not char.isalnum():
+                words = words.replace(char, '')
+
+        backwards = words[::-1]
+
+        if words == backwards:
+            palindrome = True
+            
+        return palindrome
