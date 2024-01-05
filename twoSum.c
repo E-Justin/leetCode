5f86ab1e2a94095912eebc949764cd169d84a90b
E-Justin <67754579+E-Justin@users.noreply.github.com>
2024-01-05 15:18:50 -0600

    Update twoSum.c

--- twoSum.c
+++ twoSum.c
@@ -1,22 +1,31 @@
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-	int* ret[2];
-	int done = 0;
+// 118ms
+// 7.23MB
 
-	// keep first position and loop through all other elements
-	for (int i = 0, j = i + 1, k; i < numsSize - 1 && !done; i++, j++) {
-		k = j;
-		while (k < numsSize) {
-			printf("Checking %d and %d\n", nums[i], nums[k]);
-			if (nums[i] + nums[k] == target) {
+#include <stdio.h>
+#include <stdlib.h>
+
+int*
+twoSum(int* nums, int numsSize, int target, int* returnSize) {
+	// it is the responsibility of the caller to free returned value
+
+	int* ret = (int*)malloc(sizeof(int) * 2);
+	*returnSize = 2;
+
+	// move through each element in the array if necessary 
+	for (int i = 0; i < numsSize -1; i++) {
+		// run through remaining elements
+		for (int j = i + 1; j < numsSize; j++) {
+
+			// if target was found
+			if (nums[i] + nums[j] == target) {
 				ret[0] = i;
-				ret[1] = k;
-				done = 1;
+				ret[1] = j;
 				break;
 			}
-			k++;
 		}
 	}
 
-	printf("%d, %d\n", ret[0], ret[1]);
+	// description says there is always one result, so no need for not found solution
 	return ret;
+	
 }
