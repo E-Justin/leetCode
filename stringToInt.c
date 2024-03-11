58d2d82be2b3b0772c3fd2a9fcf315dbfcbb0d35
E-Justin <67754579+E-Justin@users.noreply.github.com>
2024-03-11 07:38:27 -0500

    Update stringToInt.c

--- stringToInt.c
+++ stringToInt.c
@@ -1,24 +1,31 @@
-// 3ms
-// 5.7 MB
+// 0ms       beats 100% of c submissions
+// 5.6 MB
 
 #include <limits.h>
 #include <stdio.h>
 
-int myAtoi(char* s) {
-
-    long long int num;
+int
+myAtoi(char* s) {
+    long long num;
     int ret = sscanf(s, "%lld", &num);
 
-    // check if scanf was successful
-    if (ret <= 0) {
-        num = 0;
-    }
-    // check if int overflow
-    else if (num > INT_MAX || num < INT_MIN) {
-        // clamp int
-        num = num > INT_MAX ? INT_MAX : INT_MIN;
-    }
+    do {
+
+        // if scanf failed
+        if (ret <= 0) {
+            num = 0;
+            break;
+        }
+
+        // if outside of 32 bit range int
+        if (num > INT_MAX || num < INT_MIN) {
+            // clamp int
+            num = num > INT_MAX ? INT_MAX : INT_MIN;
+            break;
+        }
+
+    } while (0);
+
     return (int)num;
-
-
+    
 }
