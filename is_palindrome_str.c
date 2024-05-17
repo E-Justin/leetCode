6e0054085e43b41a4d5433480abd23f90bd3a521
E-Justin <67754579+E-Justin@users.noreply.github.com>
2024-05-17 08:01:14 -0500

    Update is_palindrome_str.c

--- is_palindrome_str.c
+++ is_palindrome_str.c
@@ -1,11 +1,46 @@
-// 2ms
-// 6MB
-
 #include <stdio.h>
 #include <stbool.h>
 #include <string.h>
 #include <ctype.h>
 
+// 0 ms : beats 100.00% of submissions!
+// 6 MB
+bool
+isPalindrome(char* s) {
+	char* modified = s;
+	size_t len = strlen(s);
+	bool is_palindrome = true;
+
+	for (int front = 0, end = len - 1; end > front; front++, end--) {
+
+        // while not alphanumeric, skip
+		while (!isalnum(s[front]) && end > front) {
+			front++;
+		}
+
+        // while not alphanumeric, skip
+		while (!isalnum(s[end]) && end > front) {
+			end--;
+		}
+
+        // if we have passed the middle (everything has already been compared)
+		if (front > end) {
+			break;
+		}
+		
+		// if not same char after converting to lowercase
+		if (tolower(s[front]) != tolower(s[end])) {
+			is_palindrome = false;
+			break;
+		}
+	}
+
+	return is_palindrome;
+}
+
+
+// 2ms
+// 6MB
 bool
 is_palindrome(char* s) {
 
