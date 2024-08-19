fffa0fc8cab410cfc0911c2812c2aede7a8b03cc
E-Justin <67754579+E-Justin@users.noreply.github.com>
2024-08-19 08:04:17 -0500

    Update longestCommonPrefix.c

--- longestCommonPrefix.c
+++ longestCommonPrefix.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 char* 
 longestCommonPrefix(char** strs, int strsSize) {
@@ -47,5 +48,37 @@ longestCommonPrefix(char** strs, int strsSize) {
 
     
     return ret;
+}
 
+int comp_strings(const void* a, const void* b) {
+	const char* str1 = *(const char**)a;
+	const char* str2 = *(const char**)b;
+	return strcmp(str1, str2);
+}
+
+char*
+longest_common_prefix(char** strs, int strsSize) {
+	char* ret = "";
+	unsigned int n_prefix = 0;
+
+	// sort array in ascending order
+	qsort(strs, strsSize, sizeof(char*), comp_strings);
+
+    // get size of longest common prefix
+	size_t len1 = strlen(strs[0]);
+	size_t len2 = strlen(strs[strsSize - 1]);
+	for (int i = 0; i < len1 && i < len2 && strs[0][i] == strs[strsSize - 1][i]; i++){
+		n_prefix++;
+	}
+
+    // if there is a longest common prefix
+	if (n_prefix) {
+        // allocate memory and copy the lcp
+		ret = (char*)malloc(n_prefix + 1);
+		memcpy(ret, strs[0], n_prefix);
+		ret[n_prefix] = '\0';
+	}
+
+    // ret = n_prefix ? ret : ""
+	return ret;
 }
