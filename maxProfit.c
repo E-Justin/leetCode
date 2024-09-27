d1af47a1d3e732940a2e91e78abf1755342e1bed
E-Justin <67754579+E-Justin@users.noreply.github.com>
2024-09-27 08:22:26 -0500

    Update maxProfit.c

--- maxProfit.c
+++ maxProfit.c
@@ -20,3 +20,28 @@ maxProfit(int* prices, int pricesSize) {
     return profit;
 
 }
+
+
+// 98 ms
+// 15.5 MB
+int
+max_profit(int* prices, int pricesSize) {
+	int profit = 0;
+
+	do {
+		if (pricesSize == 0) break;
+
+		int min_price = prices[0];
+
+		for (int i = 1; i < pricesSize; i++) {
+
+			if (prices[i] < min_price) min_price = prices[i];
+
+			else if (prices[i] - min_price > profit) profit = prices[i] - min_price;
+
+		}
+
+	} while (0);
+
+	return profit;
+}
