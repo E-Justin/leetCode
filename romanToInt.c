66d36667fbc5e8ae36469c84e70370e5f2677f2d
E-Justin <67754579+E-Justin@users.noreply.github.com>
2024-10-24 07:22:46 -0500

    Update romanToInt.c

--- romanToInt.c
+++ romanToInt.c
@@ -1,48 +1,67 @@
-// 3ms
-// 6.47 MB
+// Runtime: 0ms : Beats 100% of submissions
+// Memory Usage: 10.4MB
 
 int romanToInt(char* s) {
-    int len = strlen(s);
-    int curr = 0;
-    int prev = 0;
+    size_t len = strlen(s);
+    unsigned int j = 0;
     int ret = 0;
 
+    // run through each element in string
     for (int i = 0; i < len; i++) {
+    	if (i + 1 < len) j = i + 1;
+    
+    	int this_val = 0;
+    	int next_val = 0;
 
-        switch (s[i]) {
-        case 'I':
-            curr = 1;
-            break;
-        case 'V':
-            curr = 5;
-            break;
-        case 'X':
-            curr = 10;
-            break;
-        case 'L':
-            curr = 50;
-            break;
-        case 'C':
-            curr = 100;
-            break;
-        case 'D':
-            curr = 500;
-            break;
-        case 'M':
-            curr = 1000;
-            break;
-        }
+        // get this_val && next_val (if there)
+    	for (int count = 0; count < 2; count++) {
+    		char c = count == 0 ? s[i] : s[j];
+    		if (count == 1 && !j) break;
+    
+    		switch (c) {
+    		case 'I':
+    			if (count == 0) this_val = 1;
+    			else next_val = 1;
+    			break;
+    		case 'V':
+    			if (count == 0) this_val = 5;
+    			else next_val = 5;
+    			break;
+    		case 'X':
+    			if (count == 0) this_val = 10;
+    			else next_val = 10;
+    			break;
+    		case 'L':
+    			if (count == 0) this_val = 50;
+    			else next_val = 50;
+    			break;
+    		case 'C':
+    			if (count == 0) this_val = 100;
+    			else next_val = 100;
+    			break;
+    		case 'D':
+    			if (count == 0) this_val = 500;
+    			else next_val = 500;
+    			break;
+    		case 'M':
+    			if (count == 0) this_val = 1000;
+    			else next_val = 1000;
+    			break;
+    		}
+    	}
 
-        if (curr > prev && prev != 0) {
-            ret = ret + (curr - prev * 2) ;
-        }
-        else {
-            ret += curr;
-        }
-
-        prev = curr;
-
+        // if next_val is larger than current position
+    	if (next_val && next_val > this_val) {
+    		ret += (next_val - this_val);
+    		j++;
+    		i++;
+    	}
+    	else {
+    		ret += this_val;
+    	}
+    	
+    
     }
-
+    
     return ret;
 }
