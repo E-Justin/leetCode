bc31f1f1297cb042b2a5cd187a5b47a9419a5e38
E-Justin <67754579+E-Justin@users.noreply.github.com>
2025-04-18 14:00:02 -0500

    Update removeDuplicates.c

--- removeDuplicates.c
+++ removeDuplicates.c
@@ -1,22 +1,15 @@
-// 1845 ms
-// 8.38 MB
+// 902 ms
+// 12 MB
 
 int removeDuplicates(int* nums, int numsSize){
-    int k = numsSize;
-    // move through list find each duplicate
-    for (int i = 0; i < numsSize - 1; i++) {
-        int j = i + 1;
-        while (j < numsSize && nums[j] == nums[i] && nums[i] != 999) {
-            nums[j] = 999;
-            k--;
-
-            // move duplicate to end
-            for (int x = j; x < numsSize - 1; x++) {
-                int temp = nums[x];
-                nums[x] = nums[x + 1];
-                nums[x + 1] = temp;
-            }
-        }
+    for (int i = 0, j = 1; j < numsSize && nums[j] != 101; j++, i++) {
+    	while(nums[i] == nums[j] && j < numsSize){
+    		numsSize--;
+    		for (int k = j; k < numsSize; k++){
+    			nums[k] = nums[k + 1];
+    		}
+    	}
     }
-    return k;
+    
+    return numsSize;
 }
