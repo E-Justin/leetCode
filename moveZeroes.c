c3f64aac7473c6ee5cea4458cfccb6992d675524
E-Justin <67754579+E-Justin@users.noreply.github.com>
2025-05-09 08:19:59 -0500

    Update moveZeroes.c

--- moveZeroes.c
+++ moveZeroes.c
@@ -1,9 +1,37 @@
+#include <stdio.h> // printf
+#include <stdlib.h> // malloc
+#include <string.h> // memset
+
+// 0ms (beats 100% of C submissions)
+// 20 MB
+void
+m_0s(int* nums, int numsSize) {
+
+    // temp array to store !0 digits
+	int* temp = malloc(sizeof(int) * numsSize);
+	memset(temp, 0, sizeof(int) * numsSize);
+	unsigned int n_temp = 0;
+
+    // place all !0 digits into temp, in relative order
+	for (int i = 0; i < numsSize; i++) {
+		if (nums[i] != 0) {
+			temp[n_temp++] = nums[i];
+		}
+	}
+
+    // place the digits back in nums (nums needs to be modified correctly to be accepted)
+	for (int i = 0; i < numsSize; i++) {
+		nums[i] = temp[i];
+	}
+
+    // clean up
+	free(temp);
+
+    return;
+}
+
 // 295 ms
 // 15 MB
-
-#include <stdio.h>
-#include <stdlib.h>
-
 void
 moveZeroes(int* nums, int numsSize) {
     int temp;
